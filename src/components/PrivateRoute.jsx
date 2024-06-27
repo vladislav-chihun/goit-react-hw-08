b1cf5b56585b229f59cc1833dbed0eb43974ec46
vladislav-chihun <vladikkosmo@gmail.com>
2024-06-27 20:26:00 +0200

    pages fix

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,6 +1,5 @@
 import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../redux/auth/selectors";
-import ContactsPage from "./ContactsPage/ContactsPage";
 import { Navigate } from "react-router-dom";
 
 export default function PrivateRoute({component}) {
---
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -0,0 +1,11 @@
+import ContactForm from "../../components/ContactForm/ContactForm";
+import ContactList from "../../components/ContactList/ContactList"
+import SearchBox from "../../components/SearchBox/SearchBox";
+export default function ContactsPage() {
+    return <>
+        <ContactForm />
+        <SearchBox />
+        <ContactList />
+        
+    </>
+}
